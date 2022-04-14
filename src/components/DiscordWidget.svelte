0e639a5c054f96bed71d983c804efb33e6871a0e
Rahman <Rahman@seznam.cz>
2022-04-14 19:34:42 +0200

    Discord widget moved to separate component

---
+++ src/components/DiscordWidget.svelte
@@ -0,0 +1,12 @@
+<iframe class="DiscordWidget" title="Discord server" src="https://discord.com/widget?id=913419391381676102&theme=dark" width="350" height="500" allowtransparency frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
+
+<style>
+  .DiscordWidget
+  {
+    /* Center the iframe horizontally. */
+    display: block;
+
+    margin: 0 auto;
+    max-width: 100%;
+  }
+</style>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+  import About from "../data/About.md";
+  import DiscordWidget from "../components/DiscordWidget.svelte";
   import Header from "../components/Header.svelte";
   import Panel from "../components/Panel.svelte";
-  import About from "../data/About.md";
   import { guiLocalization } from "../localization/guiLocalization";
 </script>
 
@@ -13,16 +14,5 @@
   </main>
   <hr />
   <p>{guiLocalization["cs"].connectToDiscord}</p>
-  <iframe class="Discord" title="Discord server" src="https://discord.com/widget?id=913419391381676102&theme=dark" width="350" height="500" allowtransparency frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
+  <DiscordWidget />
 </Panel>
-
-<style>
-  .Discord
-  {
-    /* Center the iframe horizontally. */
-    display: block;
-
-    margin: 0 auto;
-    max-width: 100%;
-  }
-</style>
