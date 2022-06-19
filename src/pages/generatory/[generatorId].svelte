46e8bf2d1bfd7d29a0a55d9d2abd92acfee4b4ec
Rahman <Rahman@seznam.cz>
2022-06-19 23:53:30 +0200

    Handling of invalid generator url

--- src/pages/generatory/[generatorId].svelte
+++ src/pages/generatory/[generatorId].svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import Header from "../../components/Header.svelte";
   import Panel from "../../components/Panel.svelte";
+  import Markdown from "../../components/Markdown.svelte";
+  import NotFound from "../../data/NotFound.md";
+
   import Generator from "../../components/Generator.svelte";
   // import { personnalItems } from "../../data/PersonnalItems";
   import { randomTables } from "../../data/RandomTables";
@@ -13,6 +16,12 @@
 <Header />
 
 <Panel>
-  <h2>{table.heading}</h2>
-  <Generator sourceList={table.items} itemType={table.itemType} />
+  {#if table}
+    <h2>{table.heading}</h2>
+    <Generator sourceList={table.items} itemType={table.itemType} />
+  {:else}
+    <Markdown>
+      <NotFound />
+    </Markdown>
+  {/if}
 </Panel>
